8aad56763e7c3802c94d40771e531c3177702474
Bihan <banerjeebihan456@gmail.com>
2021-12-29 14:07:53 +0530

    and file completed

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 import joblib # FOR SAVING MY MODEL AS A BINARY FILE
 from matplotlib.colors import ListedColormap
 import os
-plt.style.use("fivethirtyeigh")
+plt.style.use("fivethirtyeight")
 
 def prepare_data(df):
   X = df.drop("y", axis=1)
